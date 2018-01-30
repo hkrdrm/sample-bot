895b18af2235e4a98d2223347b094b7943243332
Matt Spiers <mspiers@franklintelephone.com>
2018-01-30 08:40:51 -0600

    initial commit

--- bot.js
+++ bot.js
@@ -1,12 +1,37 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var cheerio = require('cheerio');
+var axios = require('axios');
+var _ = require('lodash');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
+function scrape(){
+  return new Promise((resolve, reject) => {
+    var str = '';
+    axios.get('https://www.fandango.com/moviesintheaters')
+      .then(function (response){
+        var $ = cheerio.load(response.data);
+        $('.movie-list').first().contents().each(function(i,elem){
+          var url = $(this).find('.visual-thumb').attr('data-src');
+          if(url){
+            str += ' ' + url + '\n';
+          }
+        })
+        resolve(str);
+      })
+      .catch(function (error) {
+        logger.error(error);
+        reject(error)
+      });
+  })
+}
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
@@ -32,6 +57,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Pong!'
                 });
+            case 'boxoffice':
+              let msg = ''
+              scrape().then((res) => {
+                msg = res
+                bot.sendMessage({
+                  to: channelID,
+                  message: msg
+                });
+              })
             break;
             // Just add any case commands if you want to..
          }
